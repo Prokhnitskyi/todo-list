d9071c74e657f96d980c69042f3455270bb24863
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-15 20:09:58 +0300

    Add TodoItem methods

--- src/todoItem.js
+++ src/todoItem.js
@@ -7,7 +7,7 @@ export class TodoItem {
     priority = 0,
     flag = false,
     completed = false,
-    tags = []
+    tags = [],
   }) {
     if (title.length === 0 && description.length === 0) {
       throw new Error('Title and description can\'t both be empty');
@@ -15,11 +15,42 @@ export class TodoItem {
 
     this.title = title;
     this.description = description;
-    this.URL = URL
+    this.URL = URL;
     this.dueDate = dueDate;
     this.priority = priority;
     this.flag = flag;
     this.completed = completed;
     this.tags = tags;
+  }
+
+  addTag (tag) {
+    if (!this.tags.includes(tag) && tag.length !== 0) {
+      this.tags.push(tag);
+    }
+  }
+
+  removeTag (tag) {
+    const array = this.tags;
+    if (array.includes(tag)) {
+      const index = array.indexOf(tag);
+      array.splice(index, 1);
+    }
+  }
+
+  switchCompletion() {
+    this.completed = !this.completed;
+  }
+
+  switchFlag() {
+    this.flag = !this.flag;
+  }
+
+  setDueDate(dateString) {
+    if (dateString.search(/\d{4}-\d{2}-\d{2}/i) !== -1) {
+      this.dueDate = new Date(dateString);
+    } else {
+      throw new Error('Invalid date format, should be yyyy-mm-dd');
+    }
+
   }
 }
