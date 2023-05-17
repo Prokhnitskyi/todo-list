81808a49a083ada658780c6c529c2c19fd22d7df
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-05-17 20:10:52 +0300

    Add navigation styling and temporary static nav

---
+++ src/components/NavigationView/Navigation.js
@@ -0,0 +1,5 @@
+export class NavigationView {
+  constructor () {
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles/style.scss';
+import './components/NavigationView/navigation.scss';
+
 import { UserInterfaceController } from './UserInterfaceController';
 
 const userInterface = new UserInterfaceController();
